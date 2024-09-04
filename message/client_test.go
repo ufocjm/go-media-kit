79909ed15d4a39b8f43e8c9d1c7d048e30cdf9b9
Debugger.C <317707256@qq.com>
2024-09-04 11:05:25 +0800

    fix:消息模块，撤回test代码

--- message/client_test.go
+++ message/client_test.go
@@ -3,7 +3,6 @@ package message_test
 import (
 	"context"
 	"github.com/heibizi/go-media-kit/message"
-	"github.com/heibizi/go-media-kit/message/iyuu"
 	"github.com/heibizi/go-media-kit/message/proxy"
 	"github.com/heibizi/go-media-kit/message/qywx"
 	"github.com/redis/go-redis/v9"
@@ -12,7 +11,6 @@ import (
 )
 
 var clientProxy *proxy.ClientProxy
-var iyuuClientProxy *proxy.ClientProxy
 
 func TestMain(m *testing.M) {
 	clientProxy = proxy.NewClientProxy(qywx.Config{
@@ -23,10 +21,6 @@ func TestMain(m *testing.M) {
 		Proxy:       os.Getenv("GO_QYWX_PROXY"),
 		UserId:      "",
 		RedisClient: redis.NewClient(&redis.Options{Addr: os.Getenv("GO_QYWX_REDIS_ADDR")}),
-	})
-	iyuuClientProxy = proxy.NewClientProxy(iyuu.Config{
-		Ctx:   context.Background(),
-		Token: os.Getenv("GO_IYUU_TOKEN"),
 	})
 	m.Run()
 }
@@ -58,51 +52,6 @@ func TestImage(t *testing.T) {
 
 func TestList(t *testing.T) {
 	err := clientProxy.Send(message.ListMessageReq{Items: []message.ImageMessageReq{
-		{TextMessageReq: message.TextMessageReq{
-			Title:   "这是一条测试消息",
-			Content: "test",
-			Link:    "https://www.baidu.com",
-		},
-			Url: "https://wwcdn.weixin.qq.com/node/wework/images/Pic_right@2x.7a03a9d992.png"},
-		{TextMessageReq: message.TextMessageReq{
-			Title:   "这是一条测试消息",
-			Content: "test",
-			Link:    "https://www.baidu.com",
-		},
-			Url: "https://wwcdn.weixin.qq.com/node/wework/images/Pic_right@2x.7a03a9d992.png"},
-	}})
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestIYUUText(t *testing.T) {
-	err := iyuuClientProxy.Send(message.TextMessageReq{
-		Title:   "这是一条测试消息",
-		Content: "test",
-		Link:    "https://www.baidu.com",
-	})
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestIYUUImage(t *testing.T) {
-	err := iyuuClientProxy.Send(message.ImageMessageReq{
-		TextMessageReq: message.TextMessageReq{
-			Title:   "这是一条测试消息",
-			Content: "test",
-			Link:    "https://www.baidu.com",
-		},
-		Url: "https://wwcdn.weixin.qq.com/node/wework/images/Pic_right@2x.7a03a9d992.png",
-	})
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestIYUUList(t *testing.T) {
-	err := iyuuClientProxy.Send(message.ListMessageReq{Items: []message.ImageMessageReq{
 		{TextMessageReq: message.TextMessageReq{
 			Title:   "这是一条测试消息",
 			Content: "test",
